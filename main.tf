38076a3306996206cb85f3e3c29a49e05ca6fcef
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-28 12:33:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,8 +72,8 @@ resource "aws_instance" "ec2_instance" {
   ami = var.ami_id
   instance_type = var.instance_type
   
-  key_name = aws_key_pair.key_pair.key_name
-  #key_name = var.key_name
+  #key_name = aws_key_pair.key_pair.key_name
+  key_name = var.key_name
   #vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   #subnet_id = 
 
