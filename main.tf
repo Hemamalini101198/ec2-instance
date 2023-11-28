0e17e6c30296790e6758f584b26bddcbe6232fa1
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-28 12:07:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_key_pair" "pub_key" {
   key_name   = var.key_name
 public_key = ""
 }
-*/
+
 
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2-sg"
@@ -67,13 +67,13 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
+*/
 resource "aws_instance" "ec2_instance" {
   ami = var.ami_id
   instance_type = var.instance_type
   
   key_name = aws_key_pair.key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  #vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   #subnet_id = 
 
   tags = {
